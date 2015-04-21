cea340961a989ecad30fb7e0773706607e604bcb
love <bridgeli45@email.com>
2015-04-21 17:54:06 +0800

    修改项目名称

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 define('APP_DEBUG',1);
 define("APP_PATH",dirname(__FILE__));
-define("LP_PATH",APP_PATH.'/bin/lovephp');
-require(APP_PATH."/bin/lovephp/init.php");
+require(APP_PATH."/bin/supermvc/init.php");
 daung();//启动项目
