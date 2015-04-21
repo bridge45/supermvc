f064242b144341352b5a8f0e871d37170dadb4e0
love <bridgeli45@email.com>
2015-04-21 17:52:32 +0800

    引入宏定义新路径模式

--- bin/supermvc/init.php
+++ bin/supermvc/init.php
@@ -1,16 +1,35 @@
 <?php
 //定义系统路径
-require LP_PATH.'/Core/Sfun/common.php';//通用用户函数库
-require LP_PATH.'/Core/Sfun/function.php';//通用系统函数库
-require LP_PATH.'/Core/Sfun/filter.php';//通用系统函数库
-require LP_PATH.'/Pmvc/Model.class.php';//数据库相关
-require LP_PATH.'/Pmvc/View.class.php';//视图
-require LP_PATH.'/Pmvc/PrivateController.class.php';//控制器协助层
-require LP_PATH.'/Pmvc/Controller.class.php';//控制器层
+define('LP_CACHE',APP_PATH.'/cachedata');
+define('LP_C',APP_PATH.'/controller');
+define('LP_M',APP_PATH.'/model');
+define('LP_V',APP_PATH.'/template');
 
-require LP_PATH.'/Core/TPL/Smarty/libs/Smarty.class.php';//控制器层
+define('LP_BIN',APP_PATH.'/bin');
+    define('LP_CON',LP_BIN.'/conf');
+    define('LP_EXT',LP_BIN.'/extent');
+    define('LP_MAIN',LP_BIN.'/supermvc');
+        define('LP_M_CORE',LP_MAIN.'/Core');
+            define('LP_MC_CACHE',LP_M_CORE.'/Cache');
+            define('LP_MC_DB',LP_M_CORE.'/DB');
+            define('LP_MC_SFUN',LP_M_CORE.'/Sfun');
+            define('LP_MC_TPL',LP_M_CORE.'/TPL');
+        define('LP_M_PMVC',LP_MAIN.'/Pmvc');
 
-define("CACHE_PATH",APP_PATH.'/cachedata');
+//加载系统配置
+require LP_MC_SFUN.'/common.php';//通用用户函数库
+require LP_MC_SFUN.'/function.php';//通用系统函数库
+require LP_MC_SFUN.'/filter.php';//通用系统函数库
+
+//MVC基类
+require LP_M_PMVC.'/Model.class.php';//数据库相关
+require LP_M_PMVC.'/View.class.php';//视图
+require LP_M_PMVC.'/PrivateController.class.php';//控制器协助层
+require LP_M_PMVC.'/Controller.class.php';//控制器层
+
+//视图框架
+require LP_MC_TPL.'/Smarty/libs/Smarty.class.php';//控制器层
+
 define('UC',Q('get.'.C('controller_tag'),C('default_controller')));//控制器
 define('UA', Q('get.'.C('action_tag'),C('default_action')));//动作
 debug_error_check();//调试模式检测
