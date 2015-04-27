ab59de2a02e80057be91986c7c056686af3c4d9a
love <bridgeli45@email.com>
2015-04-27 19:49:11 +0800

    增加S函数,session,cookie函数

--- bin/supermvc/Core/Sfun/common.php
+++ bin/supermvc/Core/Sfun/common.php
@@ -84,11 +84,95 @@ function Q($qstr, $default = '', $empty_msg = null, $filter = null, $filter_msg 
     return $Q[$name];
 }
 
+
+/**
+ * 缓存管理
+ * @param mixed $name 缓存名称，如果为数组表示进行缓存设置
+ * @param mixed $value 缓存值
+ * @param mixed $options 缓存参数
+ * @return mixed
+ */
+function S($key,$value='',$expire=null) {
+    if(isset($_GET['_clear_cache'])&&$_GET['_clear_cache']){//更新缓存机智
+        if(!isset($options['clear']))
+            S($key,null,array('clear'=>1));
+    }
+    if(isset($_GET['_flush_cache'])&&$_GET['_flush_cache']) return false;//绕过缓存机制
+
+    //单例
+    static $cache   =   '';
+    $cache || $cache = Cache::getInstance();
+    if(''=== $value){ // 获取缓存
+        return $cache->get($key);
+    }elseif(is_null($value)) { // 删除缓存
+        return $cache->rm($key);
+    }else { // 缓存数据
+        return $cache->set($key, $value, $expire);
+    }
+}
+
+
+/*
+ * session管理
+ */
+function session($key = '',$val = ''){;
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    $prefix = C('session.prefix');
+    if ('' === $key)//获取全部
+        return $_SESSION[$prefix];
+    if($key === null){
+        unset($_SESSION[$prefix]);
+    }
+    if ('' === $val)//获取$key
+        return isset($_SESSION[$prefix][$key]) ? $_SESSION[$prefix][$key] : '';
+    if (is_null($val)) {//删除session
+        unset($_SESSION[$prefix][$key]);
+    }
+    $_SESSION[$prefix][$key] = $val;
+    return TRUE;
+}
+
+/*
+ * cookie管理
+ */
+
+function cookie($key = '',$val = ''){
+    $con = C('cookie');
+    if(!empty($con['httponly'])){
+        ini_set("session.cookie_httponly", 1);
+    }
+    if ('' === $key)//获取全部
+        return $_COOKIE;
+    if($key === null){//删除全部
+        foreach ($_COOKIE as $key => $tpm) {
+            if (0 === strpos($key, $con['prefix']) || $val) {
+                setcookie($key, '', time() - 3600, $con['path'], $con['domain']);
+                unset($_COOKIE[$key]);
+            }
+        }
+        return true;
+    }
+
+    $key = $con['prefix'] ? $con['prefix'].'_'.$key : $key;
+    if ('' === $val)//获取$key
+        return isset($_COOKIE[$key]) ? $_COOKIE[$key] : '';
+    if (is_null($val)) {//删除session
+        setcookie($key, '', time() - 3600, $con['path'], $con['domain']);
+        unset($_COOKIE[$key]);
+    }
+    $expire = !empty($con['expire']) ? time() + intval($con['expire']) : 0;
+    setcookie($key, $val, $expire, $con['path'], $con['domain']);
+    return TRUE;
+}
+
 /*
  * 显示错误
  */
 function showError($msg){
-    echo 'showError'.$msg;
+
+    require LP_MC_Err.'/error.php';
     exit;
 }
 
