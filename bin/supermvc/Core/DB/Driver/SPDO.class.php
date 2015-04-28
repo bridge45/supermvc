beaaf5088358bd519acadbc451cdcfed0c4ff98a
love <bridgeli45@email.com>
2015-04-28 19:04:45 +0800

    加入调试等功能

--- bin/supermvc/Core/DB/Driver/SPDO.class.php
+++ bin/supermvc/Core/DB/Driver/SPDO.class.php
@@ -11,13 +11,32 @@ class SPDO
      */
     public $conn;
 
+    /*
+     * 预处理sql
+     */
+    protected $sql;
+
+    /*
+     * 绑定参数列表
+     */
+    protected $bindValue;
+
+    /*
+     * 调试sql
+     */
+    protected $debugSqlQueue = array();
+
     /**
      * 构造函数
      */
     public function __construct($db_con)
     {
-        $this->conn = new PDO($db_con['driver'].':host='.$db_con['host'].';dbname='.$db_con['database'].';charset=utf8',$db_con['user'],$db_con['pwd']);
-        $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);//默认还是一次传送,false改为粉刺传送
+        try {
+            $this->conn = new PDO($db_con['driver'] . ':host=' . $db_con['host'] . ';dbname=' . $db_con['database'] . ';charset=utf8', $db_con['user'], $db_con['pwd']);
+            $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);//默认还是一次传送,false改为粉刺传送
+        }catch(PDOException $Exception ){
+           Error($Exception);
+        }
     }
 
 
@@ -38,6 +57,9 @@ class SPDO
      */
     public function execute($sql,$bindValue,$mode = 1)
     {
+            $this->sql = $sql;
+            $this->bindValue = $bindValue;
+            $this->addDebugSql();
             $st = $this->conn->prepare($sql);
             if (!empty($bindValue)) {
                 foreach ($bindValue as $key => $val) {
@@ -52,36 +74,49 @@ class SPDO
      * 开始事务
      */
     public function beginTrance(){
+        $this->addDebugSql('beginTrance');
         /* 开始一个事务，关闭自动提交 */
         $this->conn->beginTransaction();
     }
 
     public function lock($lock_table_excute_code){
+        $this->addDebugSql('lock tables '.$lock_table_excute_code);
         $this->conn->exec('lock tables '.$lock_table_excute_code);
     }
 
     public function commit(){
+        $this->addDebugSql('commit');
         $this->conn->commit();
     }
 
     public function rollBack(){
+        $this->addDebugSql('rollBack');
         $this->conn->rollBack();
     }
 
+    public function  getLastSql(){
+        $string = $this->sql;
+        $data = $this->bindValue;
+        $indexed=$data==array_values($data);
+        foreach($data as $k=>$v) {
+            if(is_string($v)) $v="'$v'";
+            if($indexed) $string=preg_replace('/\?/',$v,$string,1);
+            else $string=str_replace(":$k",$v,$string);
+        }
+        return $string;
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    /*
+     * 输入调试信息
+     */
+    public function addDebugSql($sql = ''){
+        if(APP_DEBUG){
+            if(!$sql)
+                $sql = $this->getLastSql();
+            $this->debugSqlQueue['SQL'][] = $sql;
+        }
+    }
 
     /*
      * 记录sql语句
@@ -101,7 +136,9 @@ class SPDO
 
     public function free()
     {
-        //mysql_close($this->conn);
+        if(APP_DEBUG)
+            debug($this->debugSqlQueue,'group');
+        mysql_close($this->conn);
     }
 }
 
